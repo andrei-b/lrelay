f19a9a7bf506e24e871feda34d28c44805b3df0b
Andrey Borovsky <aborovskiy@adalisk.com>
2021-03-20 13:20:56 +0300

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# lrelay
-c++ usbrelay implementation
+# USBRelay
+
+USBRelay is a C++ header-only library for managing USB electrical relay modules, distributed under the MIT License.
+USBRelay is built over the HIDAPI interface, and requires that HIDAPI libraries/headers for deploy and development.
+
+![alt text](lrelays.jpg "USB relay boards")
+
+USB relay boards are based on DCT Technology. Each board carries from 1 to 8 relay switches which may be operated independently.
+Each DCT-compatible board has a EEPROM where a 5 character identifier (user serial) may be stored by user and read back later in order to identify the board.
+Using USBRealy to control relays may be as simple as this:
+```
+#include <iostream>
+#include "usbrelay.h"
+
+int main() 
+{
+    std::vector<USBRelay::USBRelayBoard> boards = USBRelay::USBRelayBoard::enumerateKnownBoards();
+    if (boards.size() > 0) {
+        std::cout << boards.size() << " USB relay boards found!" << std::endl;
+        std::cout << "First board has " << boards.front().relayCount() << " relays." << std::endl;
+        std::cout << "Setting the first relay on the first board to the ON state." << std::endl;
+        boards.front().relayOn(0);
+    } else {
+        std::cout << "No USB relay boards found!" << std::endl;
+    }
+    return EXIT_SUCCESS;
+}
+```
--- usbrelay.h
+++ usbrelay.h
@@ -1,6 +1,31 @@
-//
-// Created by andrey on 3/14/21.
-//
+/*
+
+USBRelay is a C++ header-only library for managing USB electrical relay modules
+USBRelay is built over the HIDAPI interface.
+ 
+Copyright (c) 2021 Andrey Borovsky
+
+This library is distributed under the MIT License:
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ */
 
 #ifndef _USBRELAY_H
 #define _USBRELAY_H
@@ -14,37 +39,22 @@ namespace USBRelay {
 
 #include <hidapi/hidapi.h>
 
-
-
-
     class USBRelayBoard {
     public:
-        static std::vector<USBRelayBoard> enumerateKnownBoards(unsigned short vid = 0, unsigned short pid = 0) {
+        static std::vector<USBRelayBoard> enumerateKnownBoards(unsigned short vid = 0, unsigned short pid = 0)
+        {
             struct hid_device_info *devs;
             devs = hid_enumerate(vid, pid);
             std::vector<USBRelayBoard> boards;
             auto idev = devs;
             while (idev != nullptr) {
                 if (relayType(idev) != RelayType::None) {
-                    boards.emplace_back(idev);
+                    boards.push_back(USBRelayBoard(idev));
                 }
                 idev = idev->next;
             }
             hid_free_enumeration(devs);
             return boards;
-        }
-        USBRelayBoard(struct hid_device_info *dinfo) :
-                _type(relayType(dinfo)),
-                _path(dinfo->path),
-                _serialNumber(dinfo->serial_number),
-                _releaseNumber(dinfo->release_number),
-                _manufacturerString(dinfo->manufacturer_string),
-                _vendorId(dinfo->vendor_id),
-                _productId(dinfo->product_id) {
-            if (_type == RelayType::DCT)
-                numRelays = std::stoi(std::wstring(dinfo->product_string).substr(8));
-            else
-                numRelays = _type == RelayType::UCR ? 9 : 0;
         }
 
         [[nodiscard]] const std::string &path() const { return _path; }
@@ -59,9 +69,9 @@ namespace USBRelay {
 
         [[nodiscard]] unsigned short productId() const { return _productId; }
 
-        [[maybe_unused]] int relayCount() const { return numRelays; }
+        [[nodiscard]] int relayCount() const { return numRelays; }
 
-        bool relayOpen(int num)
+        bool relayOff(int num)
         {
             if (num >= relayCount())
                 return true;
@@ -69,38 +79,43 @@ namespace USBRelay {
             return states & (1<<num) == 0;
         }
 
-        bool relayClosed(int num) {return !relayOpen(num);}
+        bool relayOn(int num) {return !relayOff(num);}
 
-        void setRelayState(int num, bool closed)
+        void setRelayState(int num, bool on)
         {
             if (num < numRelays)
-                setState(num, closed ? (uint8_t) 0xff : (uint8_t) 0xfd);
+                setState(num, on ? CmdOn : CmdOff);
         }
 
-        void openRelay(int num)
+        void setRelayOff(int num)
         {
             if (num < numRelays)
-                setState(num, 0xfd);
+                setState(num, CmdOff);
         }
 
-        void closeRelay(int num)
+        void setRelayOn(int num)
         {
             if (num < numRelays)
-                setState(num, 0xff);
+                setState(num, CmdOn);
         }
 
-        void openAllRelays()
+        void setAllRelaysOff()
         {
-            for (int i = 0; i < numRelays; ++i)
-                openRelay(i);
+            setState(0, CmdAllOff);
         }
+
+        void setAllRelaysOn()
+        {
+            setState(0, CmdAllOn);
+        }
+
 
         void toggleRelay(int n)
         {
-            if (relayClosed(n))
-                openRelay(n);
+            if (relayOn(n))
+                setRelayOff(n);
             else
-                closeRelay(n);
+                setRelayOn(n);
         }
         const std::string  &userSerial()
         {
@@ -142,7 +157,27 @@ namespace USBRelay {
         std::string _userSerial;
         static const int SerialLength = 5;
         static const int StateOffset = 7;
-        static const int CmdGetFeatures = 1;
+        static const uint8_t CmdGetFeatures = 1;
+        static const uint8_t CmdAllOn = 0xfe;
+        static const uint8_t CmdAllOff = 0xfc;
+        static const uint8_t CmdOn = 0xff;
+        static const uint8_t CmdOff = 0xfd;
+        static const uint8_t CmdSetSerial = 0xfa;
+    private:
+        USBRelayBoard(struct hid_device_info *dinfo) :
+                _type(relayType(dinfo)),
+                _path(dinfo->path),
+                _serialNumber(dinfo->serial_number),
+                _releaseNumber(dinfo->release_number),
+                _manufacturerString(dinfo->manufacturer_string),
+                _vendorId(dinfo->vendor_id),
+                _productId(dinfo->product_id) {
+            if (_type == RelayType::DCT)
+                numRelays = std::stoi(std::wstring(dinfo->product_string).substr(8));
+            else
+                numRelays = _type == RelayType::UCR ? 9 : 0;
+        }
+
         static RelayType relayType(struct hid_device_info *dinfo) {
             if (std::wstring(dinfo->product_string).find(L"USBRelay") == 0)
                 return RelayType::DCT;
@@ -170,7 +205,6 @@ namespace USBRelay {
             DeviceHandle dh(_path.c_str());
             if (_type == RelayType::DCT) {
                 uint8_t buf[9]{0, state, (uint8_t)(num+1), 0, 0, 0, 0, 0, 0};
-                printf(".calling.");
                 if (auto res = hid_write(dh.handle(), buf, sizeof (buf)); res < 0) {
                     printf(".%i.", res);
                 }
@@ -178,7 +212,6 @@ namespace USBRelay {
                 uint8_t b = (state != 0 ? (uint8_t)0xf0 : (uint8_t)0) + (uint8_t)num;
                 uint8_t buf[9]{0, b, 0, 0, 0, 0, 0, 0, 0};
                 if (auto res = hid_write(dh.handle(), buf, sizeof (buf)); res < 0) {
-
                 }
             }
         }
